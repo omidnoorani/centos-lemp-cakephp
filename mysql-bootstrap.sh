bd41953b7706be86f229b6f42db28fd088b5f797
Thijs Scheepers <tscheepers@users.noreply.github.com>
2014-02-21 11:32:54 +0100

    Create mysql-bootstrap.sh

---
+++ mysql-bootstrap.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+##
+# Vagrant bootstrap script
+#
+# Can be used to bootstrap a minimal CentOS machine to be used
+# for a CakePHP application
+# 
+# Script by Thijs Scheepers
+# Copyright 2013 Label305 B.V. All rights reserved.
+##
+
+rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+yum install mysql mysql-server -y
+
+##
+# SETUP MYSQL
+#
+# We create a vagrant mysql user with a simple password
+# It deletes the anonymous user and disables remote root access
+##
+
+chkconfig --levels 235 mysqld on
+service mysqld start
+
+mysql -u root <<"EOF"
+GRANT ALL PRIVILEGES ON *.* TO vagrant@'%.%.%.%' IDENTIFIED BY "vagrantmysqlpassword" WITH GRANT OPTION;
+DROP DATABASE test;
+DELETE FROM mysql.user WHERE User='root' AND Host!='localhost';
+DELETE FROM mysql.user WHERE User='';
+FLUSH PRIVILEGES;
+EOF
+
+mysqladmin -u root password 'vagrantmysqlpassword'
+
+echo "MySQL 'vagrant' user created with password 'vagrantmysqlpassword'"
