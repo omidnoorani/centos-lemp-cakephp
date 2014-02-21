f218887e15a38fddcc9db18b6fef3197b45778ff
Thijs Scheepers <tscheepers@users.noreply.github.com>
2014-02-21 11:38:35 +0100

    Update deploy-bootstrap.sh

--- deploy-bootstrap.sh
+++ deploy-bootstrap.sh
@@ -19,7 +19,7 @@ useradd -m -G www deploy # setup user
 mkdir /home/deploy/.ssh
 chmod 700 /home/deploy/.ssh
 
-mv authorized_keys /home/deploy/.ssh/authorized_keys
+echo "NO KEY" > /home/deploy/.ssh/authorized_keys
 
 chmod 400 /home/deploy/.ssh/authorized_keys
 chown deploy:deploy /home/deploy -R
