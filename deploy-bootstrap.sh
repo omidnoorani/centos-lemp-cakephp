c8772c07db7f676c607f6e4906eb9d32ca07f1fc
Thijs Scheepers <tscheepers@users.noreply.github.com>
2014-02-21 11:37:28 +0100

    Create deploy-bootstrap.sh

---
+++ deploy-bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+##
+# Vagrant bootstrap script
+#
+# Can be used to bootstrap a minimal CentOS machine to be used
+# for a CakePHP application
+# 
+# Script by Thijs Scheepers
+# Copyright 2013 Label305 B.V. All rights reserved.
+##
+
+##
+# SETUP DEPLOY USERS
+##
+
+useradd -m -G www deploy # setup user
+
+mkdir /home/deploy/.ssh
+chmod 700 /home/deploy/.ssh
+
+mv authorized_keys /home/deploy/.ssh/authorized_keys
+
+chmod 400 /home/deploy/.ssh/authorized_keys
+chown deploy:deploy /home/deploy -R
+
+echo deploy:$1 | chpasswd
+echo "deploy ALL=NOPASSWD: ALL" >> /etc/sudoers
+
+echo "Deploy user created"
