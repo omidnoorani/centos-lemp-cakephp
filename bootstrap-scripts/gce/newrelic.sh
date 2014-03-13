2a07cfd87084704656963db5d72b6470eee2e8e3
Thijs Scheepers <tscheepers@users.noreply.github.com>
2014-03-13 16:08:34 +0100

    Create newrelic.sh

---
+++ bootstrap-scripts/gce/newrelic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##
+# Make sure the "newrelic-license" metadata key is set.
+##
+
+NEWRELIC_LICENSE=$(curl http://metadata/0.1/meta-data/attributes/newrelic-license)
+
+rpm -Uvh http://download.newrelic.com/pub/newrelic/el5/i386/newrelic-repo-5-3.noarch.rpm
+
+yum install newrelic-sysmond
+
+nrsysmond-config --set license_key=$NEWRELIC_LICENSE
+
+/etc/init.d/newrelic-sysmond start
