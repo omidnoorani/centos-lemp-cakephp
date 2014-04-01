7214593cfaa4bf97c7ce0036a7daec64628fb132
Joris Blaak <joris@label305.com>
2014-04-01 20:37:19 +0200

    Update bootstrap.sh

--- google_compute_engine/bootstrap/bootstrap.sh
+++ google_compute_engine/bootstrap/bootstrap.sh
@@ -8,16 +8,16 @@ chkconfig iptables off
 yum update yum -y
 
 # Execute NGINX setup
-curl -s https://raw.github.com/Label305/vagrant-centos/master/bootstrap/nginx.sh | bash
+curl -s https://raw.github.com/Label305/centos-lemp-cakephp/master/bootstrap/nginx.sh | bash
 
 chmod -R g+rw /usr/share/nginx/html
 chown -R www:www /usr/share/nginx
 
 # Execute Deploy user setup
-curl -s https://raw.github.com/Label305/vagrant-centos/master/google_compute_engine/bootstrap/deploy.sh | bash
+curl -s https://raw.github.com/Label305/centos-lemp-cakephp/master/google_compute_engine/bootstrap/deploy.sh | bash
 
 # Setup Newrelic
-curl -s https://raw.github.com/Label305/vagrant-centos/master/google_compute_engine/bootstrap/newrelic.sh | bash
+curl -s https://raw.github.com/Label305/centos-lemp-cakephp/master/google_compute_engine/bootstrap/newrelic.sh | bash
 
 # Setup Cakephp
-curl -s https://raw.github.com/Label305/vagrant-centos/master/google_compute_engine/bootstrap/cakephp.sh | bash
+curl -s https://raw.github.com/Label305/centos-lemp-cakephp/master/google_compute_engine/bootstrap/cakephp.sh | bash
