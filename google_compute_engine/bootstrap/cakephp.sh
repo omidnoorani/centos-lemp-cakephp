1fd8f77d225115b89ec7dc650a364fa7b48b3dea
Thijs Scheepers <tscheepers@users.noreply.github.com>
2014-04-30 11:31:30 -0400

    Update cakephp.sh

--- google_compute_engine/bootstrap/cakephp.sh
+++ google_compute_engine/bootstrap/cakephp.sh
@@ -4,7 +4,9 @@
 # Create all temp folders
 ##
 
-mkdir -p /usr/share/nginx/html/app/tmp/{cache,logs}/{models,persistent}
+mkdir -p /usr/share/nginx/html/app/tmp/cache/{models,persistent}
+mkdir -p /usr/share/nginx/html/app/tmp/logs/
 
 chmod -R ugo+rwx /usr/share/nginx/html/app/tmp
+chmod -R g+w /usr/share/nginx/html/app
 chown -R www:www /usr/share/nginx/html/app
