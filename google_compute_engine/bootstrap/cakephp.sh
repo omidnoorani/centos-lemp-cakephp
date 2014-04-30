eb935f50e7543cb669f045c12ba36a25d6f20c38
Thijs Scheepers <tscheepers@users.noreply.github.com>
2014-04-30 11:28:12 -0400

    Update cakephp.sh

--- google_compute_engine/bootstrap/cakephp.sh
+++ google_compute_engine/bootstrap/cakephp.sh
@@ -7,3 +7,5 @@
 mkdir -p /usr/share/nginx/html/app/tmp/{cache,logs}/{models,persistent}
 
 chmod -R ugo+rwx /usr/share/nginx/html/app/tmp
+
+chown -R www:www /var/lib/nginx # to allow file uploading to work
