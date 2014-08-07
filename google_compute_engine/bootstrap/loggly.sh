1ce6aa916c81efeb1487acd2fc6ab2ef03c11627
Thijs Scheepers <tscheepers@users.noreply.github.com>
2014-08-07 18:28:38 +0200

    Update loggly.sh

--- google_compute_engine/bootstrap/loggly.sh
+++ google_compute_engine/bootstrap/loggly.sh
@@ -14,7 +14,57 @@
 # SETUP AND INSTALL LOGGLY
 ##
 
-$LOGGLY_TOKEN=$(curl http://metadata/0.1/meta-data/attributes/loggly-token)
+HTTPCODE=$(curl --write-out %{http_code} --silent --output /dev/null http://metadata/0.1/meta-data/attributes/loggly-subdomain)
 
-curl -O https://www.loggly.com/install/configure-linux.sh 
-sudo bash configure-linux.sh -a label305 -t $LOGGLY_TOKEN -u tscheepers
+if [ $HTTPCODE = "200" ]; then
+
+  LOGGLY_SUBDOMAIN=$(curl http://metadata/0.1/meta-data/attributes/loggly-subdomain)
+  LOGGLY_TOKEN=$(curl http://metadata/0.1/meta-data/attributes/loggly-token)
+  LOGGLY_USER=$(curl http://metadata/0.1/meta-data/attributes/loggly-user)
+  LOGGLY_PASSWORD=$(curl http://metadata/0.1/meta-data/attributes/loggly-password)
+  
+  echo 0 >/selinux/enforce
+  
+  curl -O https://www.loggly.com/install/configure-linux.sh 
+  bash configure-linux.sh -a $LOGGLY_SUBDOMAIN -t $LOGGLY_TOKEN -u $LOGGLY_USER -p$LOGGLY_PASSWORD
+  
+  #nginx nginx.conf
+  cat > /etc/rsyslog.d/21-nginx-loggly.conf <<"EOF"
+$ModLoad imfile
+$InputFilePollInterval 10 
+$PrivDropToGroup adm
+$WorkDirectory /var/spool/rsyslog
+
+# Nginx access file:
+$InputFileName /var/log/nginx/access.log
+$InputFileTag nginx-access:
+$InputFileStateFile stat-nginx-access
+$InputFileSeverity info
+$InputFilePersistStateInterval 20000
+$InputRunFileMonitor
+
+#Nginx Error file: 
+$InputFileName /var/log/nginx/error.log
+$InputFileTag nginx-error:
+$InputFileStateFile stat-nginx-error
+$InputFileSeverity error
+$InputFilePersistStateInterval 20000
+$InputRunFileMonitor
+
+#Add a tag for nginx events
+$template LogglyFormatNginx,"<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [d4208620-e22e-47bb-8b56-e5731c62e042@41058 tag=\"nginx\"] %msg%\n"
+
+if $programname == 'nginx-access' then @@logs-01.loggly.com:514;LogglyFormatNginx
+if $programname == 'nginx-access' then ~
+if $programname == 'nginx-error' then @@logs-01.loggly.com:514;LogglyFormatNginx
+if $programname == 'nginx-error' then ~
+EOF
+  
+  sed -i "s/;error_log = syslog/error_log = syslog/" /etc/php.ini
+  
+  service rsyslog restart
+  
+else 
+  echo "Loggly not setup in the metadata"
+fi
+
